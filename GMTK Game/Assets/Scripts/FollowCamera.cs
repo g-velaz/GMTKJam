6bafcd92cb74943e73539944032f069f113d09da
Gemaris <gxv170430@utdallas.edu>
2021-06-12 20:18:51 -0500

    follow camera added

--- GMTK Game/Assets/Scripts/FollowCamera.cs
+++ GMTK Game/Assets/Scripts/FollowCamera.cs
@@ -6,30 +6,35 @@ public class FollowCamera : MonoBehaviour {
 	public float interpVelocity;
 	public float minDistance;
 	public float followDistance;
+	public int test;
 	public GameObject target1;
 	public GameObject target2;
 	public Vector3 offset;
 	Vector3 targetPos;
+	private Vector3 centerOfTargets;
 	// Use this for initialization
 	void Start () {
-		target1Pos = transform.position;
-		target2Pos = transform.position;
+		targetPos = transform.position;
+		centerOfTargets = ((target1.transform.position + target2.transform.position) / 2); 
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (target)
+		if (target1)
 		{
 			Vector3 posNoZ = transform.position;
-			posNoZ.z = target.transform.position.z;
-
-			Vector3 targetDirection = (target1.transform.position - posNoZ);
+			
+			centerOfTargets = ((target1.transform.position + target2.transform.position) / 2);
+			
+			posNoZ.z = centerOfTargets.z;
+			
+			Vector3 targetDirection = (centerOfTargets - posNoZ);
 
 			interpVelocity = targetDirection.magnitude * 5f;
 
 			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime); 
 
-			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.25f);
+			transform.position = Vector3.Lerp( transform.position, targetPos + offset, 0.10f);
 		}
 	}
 }
